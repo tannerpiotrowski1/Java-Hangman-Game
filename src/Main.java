f79029e67a47570d8c80a9ee1be0f76593a60ad0
TannerPiotrowski <tannerski1@gmail.com>
2025-09-29 16:22:16 -0400

    Added auto replay feature

--- src/Main.java
+++ src/Main.java
@@ -13,78 +13,94 @@ public class Main {
 
         String filePath = "5000-more-common.txt";
         ArrayList<String> words = new ArrayList<>();
+        boolean playAgain = true;
 
+        // Ensure file is properly read
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
             String line;
             while((line = reader.readLine()) != null){
                 words.add(line.trim().toUpperCase());
             }
-
-        }catch(FileNotFoundException e){
+        }
+        catch(FileNotFoundException e){
             System.out.println("COULD NOT FIND FILE");
-
-        }catch(IOException e){
+        }
+        catch(IOException e){
             System.out.println("SOMETHING WENT WRONG");
         }
 
-        Random random = new Random();
-        String word = words.get(random.nextInt(words.size()));
+        while(playAgain){
+            // Chooses random word from word list
+            Random random = new Random();
+            String word = words.get(random.nextInt(words.size()));
 
 
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Character> wordState = new ArrayList<>(); // Underscores
-        int wrongGuesses = 0;
+            Scanner scanner = new Scanner(System.in);
 
-        for(int i = 0; i < word.length(); i++){ // Adds appropriate number of underscores
-            wordState.add('_');
-        }
+            // Underscores
+            ArrayList<Character> wordState = new ArrayList<>();
+            int wrongGuesses = 0;
 
-        System.out.println("************************");
-        System.out.println("Welcome to JAVA Hangman!");
-        System.out.println("************************");
-
-        while(wrongGuesses < 6){
-
-            System.out.println(getHangmanArt(wrongGuesses));
-
-            System.out.print("Word: ");
-            for(char c : wordState){
-                System.out.print(c + " ");
+            // Adds appropriate number of underscores
+            for(int i = 0; i < word.length(); i++){
+                wordState.add('_');
             }
-            System.out.println();
 
-            System.out.print("Guess a letter: ");
-            char guess = scanner.next().toUpperCase().charAt(0);
+            System.out.println("************************");
+            System.out.println("Welcome to JAVA Hangman!");
+            System.out.println("************************");
 
-            if(word.indexOf(guess) >= 0){
-                System.out.println("Correct guess!\n");
+            while(wrongGuesses < 6){
 
-                for(int i = 0; i < word.length(); i++){
-                    if(word.charAt(i) == guess){
-                        wordState.set(i, guess);
+                System.out.println(getHangmanArt(wrongGuesses));
+
+                System.out.print("Word: ");
+                for(char c : wordState){
+                    System.out.print(c + " ");
+                }
+                System.out.println();
+
+                System.out.print("Guess a letter: ");
+                char guess = scanner.next().toUpperCase().charAt(0);
+
+                if(word.indexOf(guess) >= 0){
+                    System.out.println("Correct guess!\n");
+
+                    for(int i = 0; i < word.length(); i++){
+                        if(word.charAt(i) == guess){
+                            wordState.set(i, guess);
+                        }
                     }
-                }
 
-                if(!wordState.contains('_')){
-                    System.out.print(getHangmanArt(wrongGuesses));
-                    System.out.println("YOU WIN!");
-                    System.out.println("The word was: " + word);
-                    break;
+                    if(!wordState.contains('_')){
+                        System.out.print(getHangmanArt(wrongGuesses));
+                        System.out.println("YOU WIN!");
+                        System.out.println("The word was: " + word);
+                        break;
+                    }
+                }else{
+                    wrongGuesses++;
+                    System.out.println("Wrong guess!");
                 }
-            }else{
-                wrongGuesses++;
-                System.out.println("Wrong guess!");
             }
+
+            if(wrongGuesses >= 6){
+                System.out.print(getHangmanArt(wrongGuesses));
+                System.out.println("GAME OVER!");
+                System.out.println("The word was: " + word);
+            }
+
+            System.out.println("Play again (Y/N)?: ");
+            String answer = scanner.next().toUpperCase();
+            if(answer.equals("Y")){
+                continue;
+            }else{
+                System.out.println("Thank you for playing! Goodbye!");
+                playAgain = false;
+            }
+
+            scanner.close();
         }
-
-        if(wrongGuesses >= 6){
-            System.out.print(getHangmanArt(wrongGuesses));
-            System.out.println("GAME OVER!");
-            System.out.println("The word was: " + word);
-        }
-
-
-        scanner.close();
     }
 
     static String getHangmanArt(int wrongGuesses){
